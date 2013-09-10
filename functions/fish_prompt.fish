56ab88f207fcb2f4560cfe203def73a01a18914f
Matt McKellar-Spence <matt@mattms.com>
2013-09-10 22:56:55 +1000

    Remove name and host from prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,12 +1,8 @@
 function fish_prompt
 	echo
 	set_color 888
-	echo -n (date "+%H:%M:%S") (whoami)
-	set_color 555
-	echo -n @
-	set_color 888
-	echo (hostname) (pwd)
+	pwd
 	set_color 0F0
-	echo -n '> '
+	echo -n (date "+%H:%M:%S") '> '
 	set_color normal
 end
