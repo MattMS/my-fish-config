e01bd62b07de4615e432447f0e1fcc972406c5a8
Matt McKellar-Spence <matt+git@mattms.com>
2017-09-25 12:28:08 +1000

    Add Go and GHC paths

--- config.fish
+++ config.fish
@@ -1,1 +1,11 @@
 set -U EDITOR vim
+
+set -x GOPATH $HOME/go
+set -x GOROOT $HOME/go/1.9
+
+# Set PATH so it includes user's private bin if it exists.
+if test -d $HOME/bin
+	set -x PATH $HOME/bin $PATH
+end
+
+set -x PATH $PATH $GOPATH/bin $GOROOT/bin $HOME/.cabal/bin /opt/cabal/2.0/bin /opt/ghc/8.2.1/bin
